98b2d90b49c1f5bcbbcc2626bfe81523242ac603
Tom Pivin <tommy.pivin94@gmail.com>
2023-08-08 13:27:30 -0400

    learning cryptography

---
+++ recoveryKey.js
@@ -0,0 +1,39 @@
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+const secp = require("ethereum-cryptography/secp256k1");
+
+function hashMessage(message) {
+    const messageBytes = utf8ToBytes(message);
+    const hash = keccak256(messageBytes);
+    
+
+    console.log(hash);
+    return hash;
+}
+
+module.exports = hashMessage;
+
+
+
+
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+async function signMessage(msg) {
+    const hashedMessage = hashMessage(msg);
+    return secp.sign(hashedMessage, PRIVATE_KEY, { recovery : true });
+    
+
+    }
+
+module.exports = signMessage;
+
+async function recoverKey (message, signature, recoveryBit) {
+    const hashedMessage = hashMessage(message);
+
+    const publicKey = secp.recoverPublicKey( hashedMessage, signature, recoveryBit);
+
+    return publicKey;
+}
+
+
