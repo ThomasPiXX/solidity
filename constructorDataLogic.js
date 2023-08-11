348beebff515bf4d683cfd55eefea62a46a7f497
Tom Pivin <tommy.pivin94@gmail.com>
2023-08-10 21:09:30 -0400

    learning block chain tech

---
+++ constructorDataLogic.js
@@ -0,0 +1,21 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+
+//receiving the new instance of block class created by the constructor data is parameter part of class
+    
+    constructor(data){
+        this.data = data;
+    }
+
+
+
+    toHash() {
+        return SHA256(this.data).toString();;
+    }
+}
+
+const block = new Block("");
+console.log(block.data)
+
+module.exports = Block;
