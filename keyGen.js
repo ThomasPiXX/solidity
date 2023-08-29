020836d9a0114e14130a6f18b5dafc3ac517ae62
Tom Pivin <tommy.pivin94@gmail.com>
2023-08-29 13:32:26 -0400

    new keygen logic

--- keyGen.js
+++ keyGen.js
@@ -1,35 +1,40 @@
-const {secp256k1} = require("ethereum-cryptography/secp256k1");
-const { toHex } = require("ethereum-cryptography/utils");
+const secp = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak");
+const { toHex, Utf8ToBytes, utf8ToBytes } = require("ethereum-cryptography/utils");
 
-function generateKey(){
-    const privateKey = secp256k1.utils.randomPrivateKey();
-    const publicKey = secp256k1.getPublicKey(privateKey);
 
-    const privateKeyHex = toHex(privateKey);
-    const publicKeyHex = toHex(publicKey);
+const privateKey = secp.secp256k1.utils.randomPrivateKey();
+console.log("Private Key =" + toHex(privateKey));
 
-    const adress = getAddressEthFormat(publicKey);
-    const addressHex = toHex(adress);
+const publicKey = secp.secp256k1.getPublicKey(privateKey);
+console.log("public Key bytes: " + (publicKey));
+console.log("public Key: " + toHex(publicKey));
 
-    return{
-        privateKey: privateKeyHex,
-        publicKey : publicKeyHex,
-        address: addressHex,
-    };
+
+const address = keccak256(publicKey.slice(1).slice(-20));
+console.log("address: " + "ox" + toHex(address));
+console.log("address bytes: " + address);
+
+const message = "block Transaction"
+const messageBytes = utf8ToBytes(message);
+const messageHash = keccak256(messageBytes);
+
+console.log("messge hash: " + toHex(messageHash));
+
+//signature 
+const messageSign = secp.secp256k1.sign(messageHash, privateKey);
+console.log("Signed message" + messageSign );
+console.log("recovery bit: ", messageSign.recovery);
+
+const isValid = secp.secp256k1.verify(messageSign, messageHash, publicKey);
+console.log("signature validation : " + isValid);
+
+const recoveredPublicKey = messageSign.recoverPublicKey(messageHash).toHex();
+console.log("recovered public Key from signature: " + recoveredPublicKey);
+
+if (toHex(publicKey) === recoveredPublicKey) {
+    console.log("recovered publicKey match !!")
+}else{
+    console.log("recovered publicKey dosent match ");
 }
-
-
-function getAddressEthFormat(publicKey){
-    const slicedPubKey = publicKey.slice(1);
-    const hashedPubKey = keccak256(slicedPubKey);
-    const last20Hash = hashedPubKey.slice(-20);
-    
-    return last20Hash;
-}
-
-module.exports = { generateKey };
-
-console.log(generateKey);
-
 //looking to import secp256k1 
