45a61cd031a66eb882451d833c46fa442962a24b
Tom Pivin <tommy.pivin94@gmail.com>
2023-09-27 20:40:47 -0400

    merkle tree:
    method : getRoot,getNextLayer,GetProof;

--- merkle.js
+++ merkle.js
@@ -1,4 +1,4 @@
-class MerkleTree {
+/* class MerkleTree {
     constructor(leaves, concat) {
         this.leaves = leaves;
         this.concat = concat;
@@ -61,3 +61,81 @@ class MerkleTree {
 }
 
 module.exports = MerkleTree;
+
+*/
+
+
+
+class MerkleTree {
+    constructor(leaves, concat) {
+        this.root = null;
+        this.leaves = leaves;
+        this.hash = concat;
+    }
+
+    getRoot() {
+        let arr = [...this.leaves];
+
+        while (arr.length !== 1) {
+            arr = this.getNextLayer(arr)
+        }
+
+        this.root = arr[0];
+
+        return this.root;
+    }
+
+
+    getNextLayer(arr) {
+        let newArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (i % 2 !== 0) {
+                newArr.push(this.hash(arr[i - 1], arr[i]));
+
+                if (i === arr.length - 1) {
+                    arr = newArr;
+                    newArr = [];
+                }
+            } else if (i === arr.length - 1) {
+                newArr.push(arr[i]);
+                arr = newArr;
+                newArr = [];
+            }
+        }
+
+        return arr;
+    }
+
+    getProof(index) {
+        let arr = [...this.leaves];
+        let newArr = [];
+        const proof = [];
+
+        while (arr.length !== 1) {
+            if (index % 2 === 0) {
+                if (index !== arr.length - 1) {
+                    proof.push({
+                        data: arr[index + 1],
+                        left: false
+                    })
+                }
+            } else {
+                proof.push({
+                    data: arr[index - 1],
+                    left: true
+                })
+            }
+
+            arr = this.getNextLayer(arr);
+
+            index = Math.floor(index / 2)
+        }
+
+        this.root = arr[0];
+        return proof;
+    }
+}
+
+module.exports = MerkleTree;
+
+
