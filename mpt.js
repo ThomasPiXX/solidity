bd6d434949db49be23b37ed6692632f114e194ed
Tom Pivin <tommy.pivin94@gmail.com>
2023-10-03 13:03:49 -0400

    adding insert logic  to the MPT

--- mpt.js
+++ mpt.js
@@ -13,4 +13,32 @@ class Trie {
     constructor(root) {
         this.root = new TrieNode(root)
     }
+
+insert(word) {
+    let currentNode = this.root;
+    let branchNode = null;
+    
+    for (let i = 0; i < word.length; i++) {
+        const key = word[i];
+
+        if (!currentNode.children[key]) {
+            currentNode.children[key] = new TrieNode(key);
+        } else {
+            if (branchNode) {
+                const newKey = word[i + 1];
+                if (!branchNode.children[newKey]) {
+                    branchNode.children[newKey] = new TrieNode(newKey);
+                }
+                currentNode = branchNode.children[newKey];
+                branchNode = null;
+            }
+        }
+        
+        currentNode = currentNode.children[key];
+    }
+
+    currentNode.isWord = true;
 }
+
+}
+
