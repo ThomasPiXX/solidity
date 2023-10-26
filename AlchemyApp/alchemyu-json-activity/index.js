fdc1275da3ad8a5112c08a34d6442a156d51bef1
Tom Pivin <thomas.pixx90@gmail.com>
2023-10-26 16:22:37 -0400

    testing RCP-JSON request

--- AlchemyApp/alchemyu-json-activity/index.js
+++ AlchemyApp/alchemyu-json-activity/index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const ALCHEMY_URL = "https://eth-goerli.g.alchemy.com/v2/R_cs2uMoZi4ZbwxjAEjuRXsdd0vzVuhs"
 
-axios.post(ALCHEMY_URL, {
+/*axios.post(ALCHEMY_URL, {
     jsonrpc: "2.0",
     id: 1,
     method: "eth_getBlockByNumber",
@@ -12,4 +12,15 @@ axios.post(ALCHEMY_URL, {
     ]
 }).then((response) =>{
     console.log(response.data.result);
-});
+});*/
+/*
+axios.post(ALCHEMY_URL, {
+    jsonrpc: "2.0",
+    method : "eth_getBalance",
+    params : [
+        "0x12463F7566d797a4b36517eB3A1cAFaB58f1A381"
+    ],
+    id: 0
+}).then((respone) => {
+    console.log(response.data.result);
+});*/
--- AlchemyApp/script.js
+++ AlchemyApp/script.js
@@ -6,14 +6,14 @@ const settings = {
 };
 
 const alchemy = new Alchemy(settings);
-/*
+
 // get the latest block
 const latestBlock = alchemy.core.getBlock("latest").then(console.log);
-*/
 
+/*
 // get all the sent transactions from given address
 const sentTransactions = alchemy.core.getAssetTransfers({
     fromBlock: "0x0",
     fromAddress: "0x994b342dd87fc825f66e51ffa3ef71ad818b6893",
     category: ["erc721", "external", "erc20"],
-}).then(console.log);
+}).then(console.log);*/
