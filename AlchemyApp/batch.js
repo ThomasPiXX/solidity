ca0d5d93ea45fdf2c1b8fc6260c3e5985d11586c
Tom Pivin <thomas.pixx90@gmail.com>
2023-11-03 13:01:43 -0400

    working with the reduce function

---
+++ AlchemyApp/batch.js
@@ -0,0 +1,26 @@
+async function getBalance(addresses){
+
+    const batch = [];
+
+    for(i = 0; i < addresses.length; i++){
+        const request = {
+            jsonrpc: "2.0",
+            id:i,
+            method: "eth_getBalance",
+            params: [addresses[i]]
+        }
+        batch.push(request)
+    }
+
+    const  { data } = await axios.post(url, batch)
+
+    const totalBalance = data.reduce((acc, cur) => parseInt(acc, 16) + parseInt(cur.result, 16), 0);
+
+    console.log(totalBalance);
+    
+    return totalBalance;
+
+
+}
+
+
