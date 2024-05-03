bbef7612297f5b9dba7d324128ba855afa8e4140
Tom Pivin <thomas.pixx90@gmail.com>
2024-05-03 15:11:00 -0400

    learning Hardhat

--- au-hardhat-practice/hardhat.config.js
+++ au-hardhat-practice/hardhat.config.js
@@ -4,4 +4,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  nethworks: {
+    sepolia: {
+      url: process.env.ALCHEMY_TEST_NET_RPC_URL,
+      account: [process.env.TESTNET_PRIAVE_KEY],
+    },
+  },
 };
